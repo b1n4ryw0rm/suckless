55dae87e6afa3717ecd35ee59d58da39be1bb894
b1n4ryw0rm <b1n4ryw0rm@gmail.com>
2023-09-17 23:17:32 +0530

    dwm updated

--- dwm/config.h
+++ dwm/config.h
@@ -6,12 +6,12 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Fonts:size=11" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 // static const char col_gray1[]       = "#222222";
 // static const char col_gray2[]       = "#444444";
 // static const char col_gray3[]       = "#bbbbbb";
@@ -19,7 +19,7 @@ static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
 // static const char col_cyan[]        = "#005577";
 // static const char col_urgborder[]   = "#ff0000";
 // static const char *colors[][3]      = {
-//  	/*               fg         bg         border   */
+ 	/*               fg         bg         border   */
 // 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 // 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 // 	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
@@ -28,7 +28,7 @@ static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
 #include "/home/b1n4ryw0rm/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
-static const char *tags[] = { "󰣇", "", "", "" };
+static const char *tags[] = { "󰣇", "", "", "󰙯", "󰢹" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,6 +52,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "[@]",      spiral },
 	{ "[\\]",     dwindle },
@@ -64,7 +65,7 @@ static const Layout layouts[] = {
 	{ ":::",      gaplessgrid },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	// { "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
@@ -90,15 +91,12 @@ static const char *flamcmd[]  = { "flameshot", "gui", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,						XK_n,	   spawn,	   	   SHCMD("st -e nmtui") },
-	{ MODKEY,						XK_r,	   spawn,	   	   SHCMD("rofi -show run") },	
-	{ MODKEY|ShiftMask,				XK_r,	   spawn,	   	   SHCMD("rofi -show drun") },
-	{ MODKEY,						XK_c,	   spawn,	   	   SHCMD("rofi -show calc") },
-	{ MODKEY,						XK_e,	   spawn,	   	   SHCMD("rofi -show emoji") },
-	{ Mod4Mask,						XK_c,	   spawn,	   	   SHCMD("chromium &") },
-	{ MODKEY,						XK_s,	   spawn,	   	   {.v = flamcmd } },
+	{ MODKEY,						XK_r,	   spawn,		   SHCMD("rofi -show run") },
+	{ MODKEY|ShiftMask,				XK_r,	   spawn,		   SHCMD("rofi -show drun") },
+	{ MODKEY,						XK_c,	   spawn,		   SHCMD("rofi -show calc") },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_s,	   spawn,	   	   {.v = flamcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
