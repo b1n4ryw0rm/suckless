3be7a0b0bc66f76187f56a3647d15a9d50fc3752
b1n4ryw0rm <b1n4ryw0rm@gmail.com>
2023-12-12 18:05:15 +0530

    color scheme changed

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 15;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=11" };
+static const char *fonts[]          = { "JetBrainsMOno Nerd Font:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -18,11 +18,15 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_urgborder[]   = "#ff0000";
+
+static const char col_section[] = "#7e1717";
+static const char col_border[] = "#b33030";
+static const char col_font[] = "#fffbf5";
+
 static const char *colors[][3]      = {
  	/*               fg         bg         border   */
  	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-  	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+	[SchemeSel]  = { col_font, col_gray1,  col_border  },
 };
 
 //#include /home/b1n4ryw0rm/.cache/wal/colors-wal-dwm.h
